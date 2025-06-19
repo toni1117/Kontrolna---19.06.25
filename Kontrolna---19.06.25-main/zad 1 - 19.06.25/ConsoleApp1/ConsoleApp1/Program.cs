2569b9dcc3d7a5207f896ae81c543d935adb4362
toni_rusev <164353382+toni1117@users.noreply.github.com>
2025-06-19 10:46:06 +0300

    Add files via upload

---
+++ Kontrolna---19.06.25-main/zad 1 - 19.06.25/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+        Dictionary<char, int> counts = new Dictionary<char, int>();
+
+        foreach (char ch in text)
+        {
+            if (char.IsLetter(ch))
+            {
+                char letter = char.ToLower(ch);
+                if (!counts.ContainsKey(letter))
+                {
+                    counts[letter] = 1;
+                }
+                else
+                {
+                    counts[letter]++;
+                }
+            }
+        }
+
+        foreach (char ch in text)
+        {
+            char letter = char.ToLower(ch);
+            if (char.IsLetter(ch) && counts[letter] > 1)
+            {
+                Console.WriteLine($"{letter} -> {counts[letter]}");
+                counts[letter] = 0;
+            }
+        }
+    }
+}
---
+++ Kontrolna---19.06.25-main/zad 2 - 19.06.25/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, int> players = new Dictionary<string, int>();
+
+        while (true)
+        {
+            string line = Console.ReadLine();
+
+            if (line == "game over")
+                break;
+
+            string[] parts = line.Split(' ');
+            string name = parts[0];
+            int points = int.Parse(parts[1]);
+
+            if (!players.ContainsKey(name))
+            {
+                players[name] = 0;
+            }
+
+            players[name] += points;
+        }
+
+        Console.WriteLine("");
+        
+        foreach (var pair in players)
+        {
+            Console.WriteLine($"{pair.Key} -> {pair.Value}");
+        }
+    }
+}
